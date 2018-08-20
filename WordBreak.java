55ef7e544c6db908d5f03e61154712a2b2067c25
kevincwu0 <kevincwu0@gmail.com>
2018-08-20 14:00:39 -0700

    Add WordBreak

---
+++ WordBreak.java
@@ -0,0 +1,88 @@
+// LeetCode #139 Word Break
+
+// 1. Brute Force Attempt
+// Recursion and BackTracking
+// O(n^n) s = "aaaaaaaa" -> every prefix
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return wordBreakHelper(s, new HashSet(wordDict), 0);
+    }
+    public boolean wordBreakHelper(String s, Set<String> wordDict, int start) {
+        if (start == s.length()) {
+            return true;
+        }
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (wordDict.contains(s.substring(start, end)) && wordBreakHelper(s, wordDict, end)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+// 2. Recursion with Memoization
+// O(n^2). Size of recursion tree can go up to n^2n ​2
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return word_Break(s, new HashSet(wordDict), 0, new Boolean[s.length()]);
+    }
+    public boolean word_Break(String s, Set<String> wordDict, int start, Boolean[] memo) {
+        if (start == s.length()) {
+            return true;
+        }
+        if (memo[start] != null) {
+            return memo[start];
+        }
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (wordDict.contains(s.substring(start, end)) && word_Break(s, wordDict, end, memo)) {
+                return memo[start] = true;
+            }
+        }
+        return memo[start] = false;
+    }
+}
+
+// 3. Breadth-First Search (BFS)
+// O(n^2). For every starting index, the search can continue till the end of the given string.
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet=new HashSet(wordDict);
+        Queue<Integer> queue = new LinkedList<>();
+        int[] visited = new int[s.length()];
+        queue.add(0);
+        while (!queue.isEmpty()) {
+            int start = queue.remove();
+            if (visited[start] == 0) {
+                for (int end = start + 1; end <= s.length(); end++) {
+                    if (wordDictSet.contains(s.substring(start, end))) {
+                        queue.add(end);
+                        if (end == s.length()) {
+                            return true;
+                        }
+                    }
+                }
+                visited[start] = 1;
+            }
+        }
+        return false;
+    }
+}
+
+// 4. Dynamic Programming
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet=new HashSet(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
