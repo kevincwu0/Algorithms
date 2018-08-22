7eb9f2560d71e23ebf344f55116d1aa54f3e2f2e
kevincwu0 <kevincwu0@gmail.com>
2018-08-21 18:11:22 -0700

    Add QueueReconstruction

---
+++ DailyTemperatures.java
@@ -0,0 +1,14 @@
+// 739. Daily Temperatures
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] ans = new int[temperatures.length];
+        Stack<Integer> stack = new Stack();
+        for(int i = temperatures.length - 1; i >= 0; --i) {
+            while(!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) stack.pop();
+            ans[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+            stack.push(i);
+        }
+        return ans;
+    }
+}
---
+++ QueueReconstruction.java
@@ -0,0 +1,33 @@
+// 406. Queue Reconstruction by Height
+
+// Java 8
+public class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+        List<int[]> list = new LinkedList<>();
+        for (int[] p : people) {
+            list.add(p[1], p);
+        }
+        return list.toArray(new int[list.size()][]);
+    }
+}
+
+// Java 
+public class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        //pick up the tallest guy first
+        //when insert the next tall guy, just need to insert him into kth position
+        //repeat until all people are inserted into list
+        Arrays.sort(people,new Comparator<int[]>(){
+           @Override
+           public int compare(int[] o1, int[] o2){
+               return o1[0]!=o2[0]?-o1[0]+o2[0]:o1[1]-o2[1];
+           }
+        });
+        List<int[]> res = new LinkedList<>();
+        for(int[] cur : people){
+            res.add(cur[1],cur);       
+        }
+        return res.toArray(new int[people.length][]);
+    }
+}
